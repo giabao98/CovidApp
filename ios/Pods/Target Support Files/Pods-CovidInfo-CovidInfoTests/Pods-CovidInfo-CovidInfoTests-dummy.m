9c0aa0ecf7b70b0ab404f83587835643ab88f639
Gia Bao <trangiabao98@gmail.com>
2021-01-26 20:56:11 +0700

    v1.6 a

--- ios/Pods/Target Support Files/Pods-CovidInfo-CovidInfoTests/Pods-CovidInfo-CovidInfoTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CovidInfo_CovidInfoTests : NSObject
-@end
-@implementation PodsDummy_Pods_CovidInfo_CovidInfoTests
-@end
--- ios/Pods/Target Support Files/Pods-CovidInfo/Pods-CovidInfo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CovidInfo : NSObject
-@end
-@implementation PodsDummy_Pods_CovidInfo
-@end
